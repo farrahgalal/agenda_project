85eb6de6d601409059f5459e7147cb4c1e305ae0
farrahg <fgalal@uci.edu>
2025-02-26 21:44:23 -0800

    created file for lookup_agenda.py fr parsing and lookups in the table

---
+++ lookup_agenda.py
@@ -0,0 +1,11 @@
+#lookup_agenda.py
+#This program finds agenda sessions in the data you imported.
+
+#To complete this task, you will need to:
+#1. Parse command line arguments to retrieve lookup conditions
+#2. Get the table records which match the lookup conditions provided
+##3. Print the resulting records onto the screen
+
+#We should be able to run your program as follows:\
+#`$> ./lookup_agenda.py column value`
+
