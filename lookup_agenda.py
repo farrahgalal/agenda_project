c30500e062d5f1acef541a2c3b7e2b8afdc37fde
farrahg <fgalal@uci.edu>
2025-02-26 23:50:58 -0800

    structure for lookup function and parameters

--- lookup_agenda.py
+++ lookup_agenda.py
@@ -9,3 +9,13 @@
 #We should be able to run your program as follows:\
 #`$> ./lookup_agenda.py column value`
 
+import sys
+from db_table import db_table
+
+def lookup_agenda(column, value):
+    return 0
+
+if __name__ == "__main__":
+    column = sys.argv[1]
+    value = sys.argv[2]
+    lookup_agenda(column, value)
